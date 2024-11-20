f9c303bfebf78b9f3523788663e5e6e71200b2a4
Carlos Quinzá <cquinzaperez@gmail.com>
2024-11-20 20:27:18 +0100

    feat(maps): base components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
